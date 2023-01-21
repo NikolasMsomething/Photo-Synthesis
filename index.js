9d4139939f25391ba8c6cef7fd87ff3464843a15
Nikolas <nikoclops@gmail.com>
2023-01-21 02:00:56 -0500

    More qol changes to code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import readline from "readline/promises";
 
 import { fetchAlbumWithId } from "./api/fetch.js";
 
-import { blue, error } from "./util/chalkColors.js";
+import { blue, error, warning } from "./util/chalkColors.js";
 import { logIdWithTitle } from "./util/helpers.js";
 import { isBetween1and100, isNumber } from "./util/validation.js";
 
@@ -22,6 +22,10 @@ const main = async () => {
 
   const shouldContinue = async () => {
     const answer = await rl.question(blue("Do you want to still keep searching albums? (y/n) "));
+
+    // If answer matches y or yes, restart application
+    // Else if answer matches n or no, kill process
+    // Else should repeat until proper input
     if (answer.match(/^y(es)?$/i)) return restart();
     else if (answer.match(/^n(o)?$/i)) return process.exit(0);
     else return shouldContinue();
@@ -30,9 +34,11 @@ const main = async () => {
   try {
     const answer = await rl.question(blue("Which picture album do you want to view? "));
 
-    // If answer is empty throw error
+    // If answer is empty, throw error
     if (!answer.length) throw new Error('Invalid input. Expected a non null input');
-    // If answer is not a number throw error
+    // If answer is exit, exit the process
+    if (answer === 'exit') return process.exit(0);
+    // If answer is not a number, throw error
     if (!isNumber(answer)) throw new Error('Invalid input. Expected a valid number input');
     // When querying for an album out of range the api returns an empty array with a 200 status, so I'm using this validation check as an extra line of precaution
     if (!isBetween1and100(answer)) throw new Error('Enter an album in the available albums range (1 - 100)');
@@ -46,10 +52,10 @@ const main = async () => {
     const albums = await response.json();
 
     // check if array of albums has records
-    // If it does not have records, log it doesnt have records and log the empty array
+    // If it does not have records, log it doesn't have records and log the empty array
     // If it does have records, log the expected `[id] title` output for each record
-    if (!albums.length) { 
-      console.log('The album you chose has no records!');
+    if (!albums.length) {
+      console.warn(warning('The album you chose has no records!'));
       console.log(albums);
     } else albums.forEach(record => logIdWithTitle(record.id, record.title));
 
--- util/chalkColors.js
+++ util/chalkColors.js
@@ -1,5 +1,7 @@
 import chalk from "chalk";
 
+// Fun terminal color variables from chalk!
 export const error = chalk.bold.red;
 export const blue = chalk.blue;
 export const correct = chalk.green;
+export const warning = chalk.yellow;
